91d3c0cfdd88f4cf0838578c23b8ca19ab846733
Edson Mendes <edson.luiz.mendes@hotmail.com>
2023-12-04 14:17:10 -0300

    first commit

---
+++ src/main/java/br/com/emendes/yourreviewapi/YourReviewApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.emendes.yourreviewapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourReviewApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourReviewApiApplication.class, args);
+	}
+
+}
