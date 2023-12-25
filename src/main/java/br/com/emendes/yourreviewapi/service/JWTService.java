cc75bc35c7029f5d7289cc6eadbf07e0a0f912f8
Edson Mendes <edson.luiz.mendes@hotmail.com>
2023-12-25 16:39:52 -0300

    feat: add filtro de autenticação via jwt

--- src/main/java/br/com/emendes/yourreviewapi/service/JWTService.java
+++ src/main/java/br/com/emendes/yourreviewapi/service/JWTService.java
@@ -1,5 +1,6 @@
 package br.com.emendes.yourreviewapi.service;
 
+import io.jsonwebtoken.JwtException;
 import org.springframework.security.core.userdetails.UserDetails;
 
 /**
@@ -9,10 +10,28 @@ public interface JWTService {
 
   /**
    * Gera um JWT de acordo com os dados em userDetails e o tempo de expiração em mili segundos.
+   *
    * @param userDetails objeto com as credenciais do usuário a quem pertencerá o jwt.
-   * @param expiration tempo (em mili segundos) de expiração do token.
+   * @param expiration  tempo (em mili segundos) de expiração do token.
    * @return String que representa o token.
    */
   String generateToken(UserDetails userDetails, long timeExpiration);
+
+  /**
+   * Verifica se o token informado é válido.
+   *
+   * @param token objeto a ser válidado.
+   * @return true caso o token seja válido, false caso contrário.
+   */
+  boolean isTokenValid(String token);
+
+  /**
+   * Extrai o subject do token.
+   *
+   * @param token objeto o qual o subject será estraído.
+   * @return o subject do JWT.
+   * @throws JwtException caso o token sejá inválido.
+   */
+  String extractSubject(String token);
 
 }
